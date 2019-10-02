cec768aef8697dd098692f516eba15e07f8593c6
Corey <iam@coreylanier.com>
2019-10-02 19:45:51 -0400

    rendering posts to the page in basic format

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home">
-    <h2>Posts</h2>
+    <p v-if="posts.length === 0">Sorry, but this blog is barren.</p>
+    <div
+      class="blog-post"
+      v-for="post in posts"
+      :key="post.id"
+    >
+      <div class="title">
+        <h2>{{post.title}}</h2>
+      </div>
+      <div class="date">
+        {{post.postDate}}
+      </div>
+      <div
+        class="body"
+        v-html="post.body"
+      />
+      <div class="comments-quantity">
+        {{post.comments.length}} comment<span v-if="post.comments.length !== 1">s</span>
+      </div>
+    </div>
   </div>
 </template>
 
