5e3167993e1f7c3c58580621d3b4a72b42077d40
Corey <iam@coreylanier.com>
2019-10-02 20:35:25 -0400

    formatting date/time to readable format

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -35,6 +35,77 @@ export default {
   async mounted() {
     await axios.get("/api/posts").then(res => {
       const posts = res.data.data;
+      posts.forEach(post => {
+        const date = new Date(post.postDate);
+        const month = date.getMonth();
+        const months = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ];
+        const days = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday"
+        ];
+        const day = date.getDay();
+        const utcDate = date.getUTCDate();
+        const year = date.getFullYear();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+
+        if (hour >= 12) {
+          const timeFormat = "PM";
+          if (hour > 12) {
+            const formattedHour = hour - 12;
+            if (minute < 10) {
+              const formattedMinute = `0${minute}`;
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${formattedHour}:${formattedMinute} ${timeFormat}`;
+            } else {
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${formattedHour}:${minute} ${timeFormat}`;
+            }
+          } else if (hour === 12) {
+            if (minute < 10) {
+              const formattedMinute = `0${minute}`;
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${hour}:${formattedMinute} ${timeFormat}`;
+            } else {
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${hour}:${minute} ${timeFormat}`;
+            }
+          }
+        } else {
+          const timeFormat = "AM";
+          if (hour === 0) {
+            const formattedHour = hour + 12;
+            if (minute < 10) {
+              const formattedMinute = `0${minute}`;
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${formattedHour}:${formattedMinute} ${timeFormat}`;
+            } else {
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${formattedHour}:${minute} ${timeFormat}`;
+            }
+          }
+          if (hour > 0) {
+            if (minute < 10) {
+              const formattedMinute = `0${minute}`;
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${hour}:${formattedMinute} ${timeFormat}`;
+            } else {
+              post.postDate = `${days[day]}, ${months[month]} ${utcDate}, ${year}, ${hour}:${minute} ${timeFormat}`;
+            }
+          }
+        }
+      });
       this.posts = posts;
     });
   }
