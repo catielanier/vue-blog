68a74ca9a4aacd326231cf18f42eb09b0ebabfc3
MissingPersonFGC <iam@coreylanier.com>
2019-12-03 21:03:01 -0500

    created form for editing users

--- src/views/EditUsers.vue
+++ src/views/EditUsers.vue
@@ -2,6 +2,21 @@
   <section class="user-settings">
     <h1>User Settings</h1>
     <vue-select label="username" :options="users" v-model="id"></vue-select>
+    <div class="roles" v-if="id">
+      <h2>Assign Role</h2>
+      <input type="radio" v-model="id.role" value="Admin" id="admin" />
+      <label for="admin">Admin</label>
+      <input type="radio" v-model="id.role" value="Author" id="author" />
+      <label for="author">Author</label>
+      <input type="radio" v-model="id.role" value="User" id="user" />
+      <label for="user">User</label>
+      <h2>Ban User</h2>
+      <input type="checkbox" v-model="id.banned" id="banned" />
+      <label for="banned">Banned</label>
+      <div>
+        <button @click.prevent="editUser">Submit Changes</button>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -15,8 +30,7 @@ export default {
   data() {
     return {
       users: [],
-      id: null,
-      userRole: null
+      id: null
     };
   },
   async beforeMount() {
@@ -44,5 +58,16 @@ h4,
 h5,
 h6 {
   font-family: "Bebas Neue";
+}
+input {
+  width: auto;
+  margin-right: 15px;
+}
+input:first-of-type,
+#banned {
+  margin-left: 15px;
+}
+label {
+  margin-right: 25px;
 }
 </style>
