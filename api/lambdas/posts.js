c077a8054c116a5a57f31b72c24ced673f824056
Corey Lanier <iam@coreylanier.com>
2019-12-06 18:13:33 -0500

    trying again with lambdas

---
+++ api/lambdas/posts.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { router: postRouter } = require("../routes/posts/postRoutes");
+const { MONGODB_URI } = process.env; // set up db connection
+
+mongoose.connect(MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const app = express(); // no need to route only /book requests; that's done in now.json
+
+app.use("/api/posts", postRouter); // just export the app instead of starting up the server
+
+module.exports = app;
