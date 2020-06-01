5681362ed5556b314964012e0b0b88ff35a6ff2e
Corey Lanier <iam@coreylanier.com>
2020-06-01 17:01:05 -0400

    moving to vuex store to handle central state

--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    loggingIn: false,
+    errorMessage: null,
+    user: null,
+    role: null,
+    showMenu: false,
   },
   mutations: {
-
+    loginStart: (state) => {
+      state.loggingIn = true;
+    },
+    loginStop: (state, errorMessage) => {
+      state.loggingIn = false;
+      state.errorMessage = errorMessage;
+    },
+    updateUser: (state, user) => {
+      state.user = user;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
   },
-  actions: {
-
-  }
-})
+  actions: {},
+});
