40ac186b1fda1f27a9260d89e589c63ddca4f982
Corey Lanier <iam@coreylanier.com>
2020-06-09 17:05:54 -0400

    creating route for handling image uploads

---
+++ api/_routes/image/imageRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const middleWare = require("../../_middleware");
+const { applyMiddleware } = require("../../_utils");
+
+applyMiddleware(middleWare, router);
+
+router.route("/").post(async (req, res) => {
+  console.log(req);
+});
+
+exports.router = router;
