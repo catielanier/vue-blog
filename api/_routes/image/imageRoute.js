120422772044946671516a3ead3bced13e9f96f7
Corey Lanier <iam@coreylanier.com>
2020-06-09 18:01:53 -0400

    incorporating multipart/form-data handling

--- api/_routes/image/imageRoute.js
+++ api/_routes/image/imageRoute.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const middleWare = require("../../_middleware");
 const { applyMiddleware } = require("../../_utils");
 
 applyMiddleware(middleWare, router);
 
 router.route("/").post(async (req, res) => {
-  console.log(req);
+  console.log(req.param);
 });
 
 exports.router = router;
