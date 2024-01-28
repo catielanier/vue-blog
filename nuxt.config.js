b186d0b06ea38a0365b2818b634dbcc037d85a78
Catie Lanier <hello@catielanier.ca>
2024-01-27 22:16:15 -0500

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: "Blog [Corey Lanier]",
+    title: "Blog [Catie Lanier]",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
